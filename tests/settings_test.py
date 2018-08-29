4884e600e34528775ef0a5df392f7001480a142d
SayMurZippowich <31690574+SayMurZippowich@users.noreply.github.com>
2018-08-29 03:08:22 +0300

    Add files via upload

---
+++ tests/settings_test.py
@@ -0,0 +1,20 @@
+from demo_telegrambot.settings import *
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+class DisableMigrations(object):
+
+    def __contains__(self, item):
+        return True
+
+    def __getitem__(self, item):
+        return "notmigrations"
+
+MIGRATION_MODULES = DisableMigrations()
